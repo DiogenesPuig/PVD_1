7e07aa78e03971e01cd262d7e06fce38c0b42723
DiogenesPuig <diogenespuig@gmail.com>
2020-09-25 11:50:59 -0300

    Iniciando el proyecto(models cargados)

---
+++ Zoo/Zoo/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from Zoo.models import *
+
+# Register your models here.
+
+admin.site.register(Personal)
+admin.site.register(Cuidador)
+admin.site.register(Limpiador)
+admin.site.register(Sector)
+admin.site.register(Animal)
